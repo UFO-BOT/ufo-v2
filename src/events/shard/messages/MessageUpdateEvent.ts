c7186953a928b94745fcf84d4a4127a8b1a08c50
SmokyPlay <smokyplay.kr@gmail.com>
2021-05-27 13:09:27 +0300

    Remove test message

--- src/events/shard/messages/MessageUpdateEvent.ts
+++ src/events/shard/messages/MessageUpdateEvent.ts
@@ -9,6 +9,6 @@ export default class MessageUpdateEvent extends AbstractEvent implements IEvent 
     public name = 'messageUpdate'
 
     public async execute(oldmsg: Discord.Message, newmsg: Discord.Message): Promise<any> {
-        if (newmsg.author.id === '591321756799598592') newmsg.reply(`нахуя ты изменил свое сообщение с ${oldmsg.content} на ${newmsg.content} ?`)
+
     }
 }
