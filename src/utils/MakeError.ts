60b99ae6f5729a4209e2924c873d5962f8c16520
Smoky <smokyplay.kr@gmail.com>
2022-08-17 00:34:34 +0300

    feat(errors): added userCoolDown error

--- src/utils/MakeError.ts
+++ src/utils/MakeError.ts
@@ -1,8 +1,17 @@
 import {EmbedBuilder, GuildMember} from "discord.js";
 import Language from "@/types/Language";
 import errors from "@/properties/errors.json";
+import TimeParser from "@/utils/TimeParser";
 
 export default class MakeError {
+    static userCoolDown(member: GuildMember, time: number, lang: Language): EmbedBuilder {
+        let error = errors.userCooldown[lang];
+        return new EmbedBuilder()
+            .setColor(process.env.SYSTEM_COLOR)
+            .setAuthor({name: error.embed.author, iconURL: member.displayAvatarURL()})
+            .setDescription(error.embed.description.replace("{{time}}", `<t:${Math.floor(time/1000)}:R>`));
+    }
+
     static other(member: GuildMember, lang: Language, text: string, name?: string): EmbedBuilder {
         let error = errors.other[lang];
         return new EmbedBuilder()
