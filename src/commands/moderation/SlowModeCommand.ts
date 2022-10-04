69c84ba750f46ff99b1abb60994c52d1958f75f5
Smoky <smokyplay.kr@gmail.com>
2022-10-04 21:14:54 +0300

    feat(moderation): added slowmode command

---
+++ src/commands/moderation/SlowModeCommand.ts
@@ -0,0 +1,71 @@
+import Discord, {ApplicationCommandOptionType, EmbedBuilder, GuildMember, PermissionResolvable} from "discord.js";
+
+import AbstractCommand from "../../abstractions/commands/AbstractCommand";
+import Command from "../../types/commands/Command";
+import CommandOption from "@/types/commands/CommandOption";
+import CommandCategory from "@/types/commands/CommandCategory";
+import CommandExecutionContext from "@/types/commands/CommandExecutionContext";
+import CommandExecutionResult from "@/types/commands/CommandExecutionResult";
+import CommandOptionValidationType from "@/types/commands/CommandOptionValidationType";
+import WarnAction from "@/services/moderation/actions/WarnAction";
+import KickAction from "@/services/moderation/actions/KickAction";
+import TimeParser from "@/utils/TimeParser";
+
+interface SlowModeCommandDTO {
+    time: number
+}
+
+export default class SlowModeCommand extends AbstractCommand implements Command {
+    public config = {
+        ru: {
+            name: "слоумод",
+            description: 'Активирует медленный режим теукщем в канале на указанное количество времени',
+            aliases: ['медленный-режим', 'медленка', 'мр']
+        },
+        en: {
+            name: "slowmode",
+            description: 'Activates a slowmode in current channel for specified amount of time',
+            aliases: ['sm']
+        }
+    }
+    public options: Array<CommandOption> = [
+        {
+            type: ApplicationCommandOptionType.String,
+            validationType: CommandOptionValidationType.Duration,
+            name: "time",
+            config: {
+                ru: {
+                    name: "время",
+                    description: "Время между сообщениями"
+                },
+                en: {
+                    name: "time",
+                    description: "Time between messages"
+                }
+            },
+            required: true
+        }
+    ]
+    public category = CommandCategory.Moderation;
+    public defaultMemberPermissions: PermissionResolvable = ["ManageChannels"];
+    public deferReply = true;
+
+    public async execute(ctx: CommandExecutionContext<SlowModeCommandDTO>): Promise<CommandExecutionResult> {
+        if(ctx.args.time > 21600000) return {
+            error: {
+                type: "other",
+                options: {text: ctx.response.data.errors.tooLongDuration}
+            }
+        }
+        await ctx.channel.setRateLimitPerUser(ctx.args.time/1000)
+        ctx.response.parse({
+            emoji: this.client.cache.emojis.slowmode,
+            slowmode: TimeParser.stringify(ctx.args.time, ctx.settings.language.interface)
+        })
+        let embed = new EmbedBuilder()
+            .setColor(this.constants.colors.system)
+            .setAuthor({name: ctx.response.data.embed.author, iconURL: ctx.member.displayAvatarURL()})
+            .setDescription(ctx.response.data.embed.description)
+        return {reply: {embeds: [embed]}}
+    }
+}
