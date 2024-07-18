9c347e83d20f6f5d9eb5f673db53dd54943a8f6e
Smoky <smokyplay.kr@gmail.com>
2024-07-18 23:07:28 +0300

    feat(api): added shards guard which rejects all api requests until all shards have been created

--- src/api/guards/shards.guard.ts
+++ src/api/guards/shards.guard.ts
@@ -10,7 +10,9 @@ import Base from "@/abstractions/Base";
 export class ShardsGuard extends Base implements CanActivate {
 
     async canActivate(context: ExecutionContext): Promise<boolean> {
-        if (!this.manager.shards.size)
+        let request = context.switchToHttp().getRequest()
+        if (!this.manager.shards.size ||
+            (this.manager.shards.size < Number(process.env.TOTAL_SHARDS) && request.path.includes('private')))
             throw new InternalServerErrorException("Shards are still being created, please wait")
         return true
     }
